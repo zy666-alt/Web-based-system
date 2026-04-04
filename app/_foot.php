4909d0733eb919b75c96886f29417a63f230d1f3
zy666-alt <chungzy-pm25@student.tarc.edu.my>
2026-04-04 17:54:34 +0800

    Add files via upload

---
+++ app/_foot.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" href="/css/app.css"> 
+    
+    </main>
+
+    <footer class="site-footer">
+        <div class="footer-container">
+            
+            <div class="footer-col">
+                <h3>Get to Know Us</h3>
+                <ul>
+                    <li><a href="#">About S&H</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h3>All Menus</h3>
+                <ul>
+                    <li><a href="#">Coffee</a></li>
+                    <li><a href="#">Boba</a></li>
+                    <li><a href="#">Tea</a></li>
+                    <li><a href="#">Juice</a></li>
+                    <li><a href="#">Chocalate</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h3>Follow Us</h3>
+                <ul>
+                    <li><a href="https://www.instagram.com/?hl=en">Instagram</a></li>
+                    <li><a href="https://www.facebook.com/">Facebook</a></li>
+                    <li><a href="https://www.xiaohongshu.com/explore">Rednote</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h3>Customer Service</h3>
+                <ul>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">FAQ's</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            <p class="copyright-text">© 1996-2026, S&H Drinks.com</p>
+        </div>
+    </footer>
+</body>
+</html>
