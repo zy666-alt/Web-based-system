a2bd2f2655ce134d3cd482158b36879e75fafb76
tingg1509 <tingg1509@gmail.com>
2026-04-13 12:02:27 +0800

    Update payment.php

--- app/payment.php
+++ app/payment.php
@@ -4,20 +4,20 @@ include '_base.php';
 $order_id = $_GET['order_id'] ?? 0;
 
 // get order total
-$stm = $_db->prepare("SELECT total_amount FROM orders WHERE order_id = ?");
+$stm = $_db->prepare("SELECT total FROM orders WHERE order_id = ?");
 $stm->execute([$order_id]);
 $order = $stm->fetch();
 
 if (!$order) 
-  temp(info,'Invalid order');
-exit
+  temp('error','Invalid order');
+exit;
 
 if (is_post()) {
     $method = req('method');
 
     // insert payment
     $stm = $_db->prepare("
-        INSERT INTO payment (order_id, amount, method, status, payment_date)
+        INSERT INTO payment (order_id, total_amount, method, status, payment_date)
         VALUES (?, ?, ?, 'Paid', NOW())
     ");
     $stm->execute([$order_id, $order->total_amount, $method]);
