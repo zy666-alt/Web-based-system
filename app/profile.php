21421fa2ed6a6d2855ae593af3466cee11c64974
Angelacjm <mjchinangela@gmail.com>
2026-04-16 15:57:12 +0800

    profile.php

--- app/profile.php
+++ app/profile.php
@@ -3,7 +3,93 @@ include '../_base.php';
 
 // ----------------------------------------------------------------------------
 
+// Authenticated users
+auth();
 
+if (is_get()) {
+    $stm = $_db->prepare('SELECT * FROM user WHERE id = ?');
+    $stm->execute([$_user->id]);
+    $u = $stm->fetch();
+
+    if (!$u) {
+        redirect('/');
+    }
+
+    extract((array)$u);
+    $_SESSION['photo'] = $u->photo;
+}
+
+if (is_post()) {
+    $email = req('email');
+    $name  = req('name');
+    $photo = $_SESSION['photo'];
+    $f = get_file('photo');
+
+    // Validate: email
+    if ($email == '') {
+        $_err['email'] = 'Required';
+    }
+    else if (strlen($email) > 100) {
+        $_err['email'] = 'Maximum 100 characters';
+    }
+    else if (!is_email($email)) {
+        $_err['email'] = 'Invalid email';
+    }
+    else {
+        $stm = $_db->prepare('
+            SELECT COUNT(*) FROM user
+            WHERE email = ? AND id != ?
+        ');
+        $stm->execute([$email, $_user->id]);
+
+        if ($stm->fetchColumn() > 0) {
+            $_err['email'] = 'Duplicated';
+        }
+    }
+
+    // Validate: name
+    if ($name == '') {
+        $_err['name'] = 'Required';
+    }
+    else if (strlen($name) > 100) {
+        $_err['name'] = 'Maximum 100 characters';
+    }
+
+    // Validate: photo (file) --> optional
+    if ($f) {
+        if (!str_starts_with($f->type, 'image/')) {
+            $_err['photo'] = 'Must be image';
+        }
+        else if ($f->size > 1 * 1024 * 1024) {
+            $_err['photo'] = 'Maximum 1MB';
+        }
+    }
+
+    // DB operation
+    if (!$_err) {
+        // (1) Delete and save photo --> optional
+        if ($f) {
+            unlink("../photos/$photo");
+            $photo = save_photo($f, '../photos');
+        }
+        
+        // (2) Update user (email, name, photo)
+        $stm = $_db->prepare('
+            UPDATE user
+            SET email = ?, name = ?, photo = ?
+            WHERE id = ?
+        ');
+        $stm->execute([$email, $name, $photo, $_user->id]);
+
+        // (3) Update global user object
+        $_user->email = $email;
+        $_user->name  = $name;
+        $_user->photo = $photo;
+
+        temp('info', 'Record updated');
+        redirect('/');
+    }
+}
 
 // ----------------------------------------------------------------------------
 
@@ -11,7 +97,27 @@ $_title = 'User | Profile';
 include '../_head.php';
 ?>
 
-<p>Removed for brevity</p>
+<form method="post" class="form" enctype="multipart/form-data">
+    <label for="email">Email</label>
+    <?= html_text('email', 'maxlength="100"') ?>
+    <?= err('email') ?>
+
+    <label for="name">Name</label>
+    <?= html_text('name', 'maxlength="100"') ?>
+    <?= err('name') ?>
+
+    <label for="photo">Photo</label>
+    <label class="upload" tabindex="0">
+        <?= html_file('photo', 'image/*', 'hidden') ?>
+        <img src="/photos/<?= $photo ?>">
+    </label>
+    <?= err('photo') ?>
+
+    <section>
+        <button>Submit</button>
+        <button type="reset">Reset</button>
+    </section>
+</form>
 
 <?php
 include '../_foot.php';
